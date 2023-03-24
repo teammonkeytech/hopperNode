d2abf11b535874ff2e16db3f9c58693a0de0a3f0
Isaac-To <isaacto3890@gmail.com>
2023-03-23 17:20:52 -0700

    refactor into classes

--- demoClient.py
+++ demoClient.py
@@ -6,28 +6,28 @@ BIT_LENGTH = 2048   # 1024, 2048, 3072 are available
 HOSTNAME = "localhost"
 PORT = "5000"
 
-def genKey(passwd):
+def genKey(pwd):
     """
     Generates a new key pair
     """
     keys = RSA.generate(BIT_LENGTH)
     with open("keys.pem", "wb") as f:
-        f.write(keys.export_key("PEM", passphrase=passwd))
+        f.write(keys.export_key("PEM", passphrase=pwd))
         f.close()
     print("New RSA Key Pair Generated")
 
-def readKey(passwd):
+def readKey(pwd):
     """
     Reads the key file and outputs the key pairing
     """
     with open("keys.pem", "rb") as f:
-        return RSA.import_key(f.read(), passphrase=passwd)
+        return RSA.import_key(f.read(), passphrase=pwd)
 
-def keyTest(passwd):
+def keyTest(pwd):
     """
     Verifies if the key functions properly.
     """
-    keys = readKey(passwd)
+    keys = readKey(pwd)
     cipher = PKCS1_OAEP.new(keys)
     from os import urandom
     testMessage = urandom(64)
@@ -49,48 +49,44 @@ def dataDecode(raw):
     from base64 import urlsafe_b64decode
     return dict(loads(urlsafe_b64decode(raw.encode("utf-8")).decode("utf-8")))
 
-def serverAuth(usn, passwd, publicKey):
-    data = {
-        "username": usn,
-        "password": passwd,
-        "publicKey": publicKey,
-    }
-    encodedData = dataEncode(data)
-    pg = requests.get(f"http://{HOSTNAME}:{PORT}/api/authenticate?data={encodedData}")
-    print(pg.text)
+class User: 
+    def __init__(self, usn):
+        self.usn = usn
+        self.uid = int(requests.get(f"http://{HOSTNAME}:{PORT}/api/user/id?data={dataEncode({'usn': usn})}").text)
+        self.pubKey = requests.get(f"http://{HOSTNAME}:{PORT}/api/user/pubKey?data={dataEncode({'usn': usn})}").text
 
-def bubbleRequest(usn):
-    # Request a list of "bubbles" a user is a part of
-    data = {
-        "username": usn
-    }
-    encodedData = dataEncode(data)
-    return requests.get(f"http://{HOSTNAME}:{PORT}/api/bubble?data={encodedData}")
 
-def messageRequest(usn, passwd, bubbleID):
-    # Request a list of messages the user can view from the bubble
-    data = {
-        "username": usn,
-        "password": passwd,
-        "bubbleID": bubbleID,
-    }
-    encodedData = dataEncode(data)
-    return requests.get(f"http://{HOSTNAME}:{PORT}/api/messages?data={encodedData}")
+class localUser(User):
+    def __init__(self, usn, pwd, keys):
+        self.pwd = pwd
+        self.keys = keys
+        User.__init__(self, usn)
+
+    def auth(self):
+        self.pubKey = self.keys.public_key().export_key().decode("utf-8")
+        data = {
+            "usn": self.usn,
+            "pwd": self.pwd,
+            "pubKey": self.pubKey,
+        }
+        encodedData = dataEncode(data)
+        pg = requests.get(f"http://{HOSTNAME}:{PORT}/api/user/auth?data={encodedData}")
+        print(pg.text)
 
 if __name__ == "__main__":
     # usn = input("Username: ")
-    # passwd = input("Password: ")
-    usn = "test"    # testing username replace when in production
-    passwd = "test" # testing password replace when in production
+    # pwd = input("Password: ")
+    usn = "test"
+    pwd = "test" # testing pwd replace when in production
     try: 
-        keyTest(passwd)
+        keyTest(pwd)
     except:
         input("Generate new keys? Reauthentication with server required")
-        genKey(passwd)
-    keys = readKey(passwd)
-    publicKey = keys.public_key().export_key().decode("utf-8")
-    serverAuth(usn, passwd, publicKey)
-    while True:
-        input("Message: ")
+        genKey(pwd)
+    keys = readKey(pwd)
+    clientUser = localUser(usn, pwd, keys=keys)
+    clientUser.auth()
+    print(clientUser.uid)
+    # bubbleRequest([0, 1])
 
     
--- server.py
+++ server.py
@@ -1,89 +1,82 @@
 import flask
 import bcrypt
-import sqlalchemy as sa
+import asyncio
+from flask_sqlalchemy import SQLAlchemy
 import json
 
 app = flask.Flask(__name__)
-engine = sa.create_engine("sqlite:///server.db") # for small servers
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///server.db"
+db = SQLAlchemy()
 
-def init():
-    # creates tables if it doesn't exist
-    metadata = sa.MetaData()
-    users = sa.Table("Users", metadata,
-                    sa.Column("uid", sa.Integer, primary_key=True, unique=True, nullable=False),
-                    sa.Column("username", sa.String(255), nullable=False),
-                    sa.Column("passwordHash", sa.String(255), nullable=False),
-                    sa.Column("publicKey", sa.String(255), nullable=False)
-                    )
-    bubbles = sa.Table("Bubbles", metadata,
-                    sa.Column("bid", sa.Integer, primary_key=True, unique=True, nullable=False),
-                    sa.Column("uid", sa.String(), nullable=True),
-                    )
-    msgs = sa.Table("Messages", metadata,
-                    sa.Column("mid", sa.Integer, primary_key=True, nullable=False),
-                    sa.Column("uidFrom", sa.Integer, nullable=False),
-                    sa.Column("uidTo", sa.Integer, nullable=False),
-                    sa.Column("timeStamp", sa.TIMESTAMP, nullable=False),
-                    sa.Column("content", sa.String(), nullable=False),
-                    )
-    metadata.create_all(engine, checkfirst=True)
+class Users(db.Model):
+    uid = db.Column("uid", db.Integer, primary_key=True, unique=True, nullable=False)
+    usn = db.Column("usn", db.String(255), nullable=False)
+    pwdHash = db.Column("pwdHash", db.LargeBinary, nullable=False)
+    pubKey = db.Column("pubKey", db.String(255), nullable=False)
+
+class Bubbles(db.Model):
+    bid = db.Column("bid", db.Integer, primary_key=True, unique=True, nullable=False)
+    uids = db.Column("uid", db.String(), nullable=True)
+
+class Messages(db.Model):
+    mid = db.Column("mid", db.Integer, primary_key=True, nullable=False)
+    authUID = db.Column("authUID", db.Integer, nullable=False)
+    bid = db.Column("bid", db.Integer, nullable=False)
+    time = db.Column("time", db.TIMESTAMP, nullable=False)
+    content = db.Column("content", db.String(), nullable=False)
 
 def decode(raw):
     from base64 import urlsafe_b64decode
     return dict(json.loads(urlsafe_b64decode(raw.encode("utf-8")).decode("utf-8")))
 
-@app.route("/api/authenticate")
-def auth():
-    """
-    auth data should include:
-    - username
-    - password
-    - new public key
-    """
+@app.route("/api/user/auth")
+async def authUser():
     raw = flask.request.args.get("data")
     data = decode(raw)
-    """
-    TODO
-    Should use the arguments in data (username, password, publicKey)
-    username and password should be used to authenticate updating of public key
-    password should be hashed before being saved to the database (use bcrypt)
-    public key should be served to clients who request it (by username) so they can
-    create encrypted messages readable only by the holder of the private key
-    """
-    return "Success"
+    query = db.select(Users).where(Users.usn == data["usn"])
+    storedUser = db.session.execute(query).fetchone()
+    if storedUser != None:
+        if bcrypt.checkpw(data["pwd"].encode("utf-8"), storedUser[0].pwdHash):
+            # update public key if new one is available
+            if storedUser[0].pubKey != data["pubKey"]:
+                storedUser[0].pubKey = data["pubKey"]
+                db.session.commit()
+                return "Key updated"
+            return "Authorized"
+        return "Invalid"
+    # add new user if key isn't in database
+    user = Users(
+        usn = data["usn"],
+        pwdHash = bcrypt.hashpw(data["pwd"].encode("utf-8"), bcrypt.gensalt()),
+        pubKey = data["pubKey"],
+    )
+    db.session.add(user)
+    db.session.commit()
+    return "Registered"
 
-@app.route("/api/bubble")
-def bubbles():
-    """
-    Returns the list of "bubbles" (chatrooms) the user is a part of
-    """
+@app.route("/api/user/id")
+async def uid():
     raw = flask.request.args.get("data")
     data = decode(raw)
-    """
-    TODO
-    Search for list of bubbles user is a part of and return that
-    """
-    bubbles = []
-    jsonedBubbles = json.dumps(bubbles)
-    return jsonedBubbles
+    query = db.select(Users).where(Users.usn == data["usn"])
+    storedUser = db.session.execute(query).fetchone()
+    if storedUser != None:
+        return str(storedUser[0].uid)
+    return f"User {data['usn']} not found"
 
-@app.route("/api/messageRequest")
-def messages():
-    """
-    Authenticate user and ensure that they are a part of the bubble
-    Returns the list of messages in the requested bubble
-    """
+@app.route("/api/bubble/new")
+async def newRoom():
     raw = flask.request.args.get("data")
     data = decode(raw)
-    """
-    Authenticate user
-    Then return messages in current bubble
-    """
-    messages = []
-    jsonedMessages = json.dumps(messages)
-    return jsonedMessages
-
+    room = Bubbles(
+        uids = json.dumps(data["uids"])
+    )
+    db.session.add(room)
+    db.session.commit()
+    return "Created"
 
 if __name__ == "__main__":
-    init()
-    app.run()
+    db.init_app(app)
+    with app.app_context():
+        db.create_all()
+    app.run() # comment out in production

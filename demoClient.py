ab6573e662d630bcbce5833171856bd5091651a1
Isaac-To <isaacto3890@gmail.com>
2023-03-24 17:55:35 -0700

    add bubble creation and invitations of new users

--- demoClient.py
+++ demoClient.py
@@ -9,6 +9,7 @@ BIT_LENGTH = 2048   # 1024, 2048, 3072 are available
 HOSTNAME = "localhost"
 PORT = "5000"
 
+
 def genKey(pwd):
     """
     Generates a new key pair
@@ -19,12 +20,14 @@ def genKey(pwd):
         f.close()
     print("New RSA Key Pair Generated")
 
+
 def readKey(pwd):
     """
     Reads the key file and outputs the key pairing
     """
     with open("keys.pem", "rb") as f:
         return RSA.import_key(f.read(), passphrase=pwd)
+
 
 def keyTest(pwd):
     """
@@ -42,18 +45,31 @@ def keyTest(pwd):
     else:
         raise Exception("KeyNotValidError")
 
-class User: 
-    def __init__(self, usn = None, uid = None):
-        if uid is not None:
-            self.usn = requests.post(f"http://{HOSTNAME}:{PORT}/api/user/usn", json={'uid': uid}).text
-            self.uid = uid
-            self.pubKey = requests.post(f"http://{HOSTNAME}:{PORT}/api/user/pubKey", json={'uid': uid}).text
-        elif usn is not None:
-            self.usn = usn
-            self.uid = int(requests.post(f"http://{HOSTNAME}:{PORT}/api/user/id", json={'usn': usn}).text)
-            self.pubKey = requests.post(f"http://{HOSTNAME}:{PORT}/api/user/pubKey", json={'usn': usn}).text
-        else:
-            raise LookupError
+
+class User:
+    def __init__(self, usn=None, uid=None, pubKey=None):
+        if usn is None:
+            usn = requests.post(
+                f"http://{HOSTNAME}:{PORT}/api/user/usn", json={'uid': uid}).text
+        if uid is None:
+            uid = int(requests.post(
+                f"http://{HOSTNAME}:{PORT}/api/user/id", json={'usn': usn}).text)
+        if pubKey is None:
+            pubKey = requests.post(
+                f"http://{HOSTNAME}:{PORT}/api/user/pubKey", json={'uid': uid}).text
+        self.usn = usn
+        self.uid = uid
+        self.pubKey = pubKey
+
+    def getUsn(self):
+        return self.usn
+
+    def getUid(self):
+        return self.uid
+
+    def getPubKey(self):
+        return self.pubKey
+
 
 class LocalUser(User):
     def __init__(self, usn, pwd, keys):
@@ -61,33 +77,68 @@ class LocalUser(User):
         self.pwd = pwd
         self.keys = keys
         self.auth()
-        User.__init__(self, usn)
+        User.__init__(
+            self, usn=usn, pubKey=self.keys.public_key().export_key().decode("utf-8"))
 
-    # convert into POST requests before deploy
+    def getPwd(self):
+        return self.pwd
+
+    def getKeys(self):
+        return self.keys
+
     def auth(self):
         self.pubKey = self.keys.public_key().export_key().decode("utf-8")
         data = {
-            "usn": self.usn,
-            "pwd": self.pwd,
-            "pubKey": self.pubKey,
+            "usn": self.getUsn(),
+            "pwd": self.getPwd(),
+            "pubKey": self.getPubKey(),
         }
-        pg = requests.post(f"http://{HOSTNAME}:{PORT}/api/user/auth", json=data)
+        pg = requests.post(
+            f"http://{HOSTNAME}:{PORT}/api/user/auth", json=data)
         print(pg.text)
 
     def sign(self, content):
         hash = SHA256.new(content.encode("utf-8"))
-        signer = PKCS115_SigScheme(self.keys)
-        return signer.sign(hash)
+        signer = PKCS115_SigScheme(self.getKeys())
+        return signer.sign(hash).hex()
+
 
 class Bubble:
-    def __init__(self, bid = None):
+    def __init__(self, bid=None):
         self.bid = bid
         self.uids = []
         if self.bid is not None:
             self.connect()
+
+    def getBid(self):
+        return self.bid
     
+    def getUids(self):
+        return self.uids
+
     def connect(self):
-        self.uids = loads(requests.post(f"http://{HOSTNAME}:{PORT}/api/bubble/uids", json = {"bid", self.bid}).text)
+        self.uids = loads(requests.post(
+            f"http://{HOSTNAME}:{PORT}/api/bubble/uids", json={"bid": self.bid}).text)
+        return self.getUids()
+
+    def invite(self, user):
+        # for when you are already connected and want to add more users
+        data = {
+            "bid": self.getBid(),
+            "uid": user.getUid()
+        }
+        status = requests.post(
+            f"http://{HOSTNAME}:{PORT}/api/bubble/invite", json=data).text
+        print(status)
+
+    def new(self, user):
+        # for when you want to create a new bubble
+        data = {
+            "uid": user.getUid(),
+        }
+        self.bid = requests.post(
+            f"http://{HOSTNAME}:{PORT}/api/bubble/new", json=data).text
+
 
 class Message:
     def __init__(self, author, bubble, content):
@@ -95,7 +146,7 @@ class Message:
         self.bubble = bubble
         self.content = content
         self.signature = author.sign(content)
-    
+
     def commit(self):
         data = {
             "authUID": self.author.uid,
@@ -103,23 +154,28 @@ class Message:
             "content": self.content,
             "sig": self.signature,
         }
-        requests.post(f"http://{HOSTNAME}:{PORT}/api/msg/commit", json=data)
+        pg = requests.post(
+            f"http://{HOSTNAME}:{PORT}/api/msg/commit", json=data)
+        print(pg)
+
 
 if __name__ == "__main__":
     # usn = input("Username: ")
     # pwd = input("Password: ")
     usn = "test"
-    pwd = "test" # testing pwd replace when in production
-    try: 
+    pwd = "test"  # testing pwd replace when in production
+    try:
         keyTest(pwd)
     except:
         input("Generate new keys? Reauthentication with server required")
         genKey(pwd)
     keys = readKey(pwd)
     clientUser = LocalUser(usn, pwd, keys=keys)
-    sessionBubble = Bubble()
-    newMessage = Message(author=clientUser, bubble=sessionBubble, content="Hello World")
-    newMessage.commit()
+    sessionBubble = Bubble(1)
+    # sessionBubble.new(clientUser.getUid())
+    altUser = User(usn="test")
+    sessionBubble.invite(altUser)
+    newMessage = Message(
+        author=clientUser, bubble=sessionBubble, content="Hello World")
+    # newMessage.commit()
     # bubbleRequest([0, 1])
-
-    
--- server.py
+++ server.py
@@ -8,27 +8,34 @@ app = flask.Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///server.db"
 db = SQLAlchemy()
 
+
 class Users(db.Model):
-    uid = db.Column("uid", db.Integer, primary_key=True, unique=True, nullable=False)
+    uid = db.Column("uid", db.Integer, primary_key=True,
+                    unique=True, nullable=False)
     usn = db.Column("usn", db.String(255), unique=True, nullable=False)
     pwdHash = db.Column("pwdHash", db.LargeBinary, nullable=False)
     pubKey = db.Column("pubKey", db.String(255), nullable=False)
 
+
 class Bubbles(db.Model):
-    bid = db.Column("bid", db.Integer, primary_key=True, unique=True, nullable=False)
-    uids = db.Column("uid", db.String(), nullable=True)
+    entid = db.Column("id", db.Integer, primary_key=True, nullable=False)
+    bid = db.Column("bid", db.Integer, nullable=False)
+    uid = db.Column("uid", db.Integer, nullable=False)
+
 
 class Messages(db.Model):
     mid = db.Column("mid", db.Integer, primary_key=True, nullable=False)
     authUID = db.Column("authUID", db.Integer, nullable=False)
     bid = db.Column("bid", db.Integer, nullable=False)
-    time = db.Column("time", db.TIMESTAMP, nullable=False)
+    time = db.Column("time", db.Time, nullable=False)
     content = db.Column("content", db.String(), nullable=False)
     sig = db.Column("sig", db.String(), nullable=False)
+
 
 def decode(raw):
     from base64 import urlsafe_b64decode
     return dict(json.loads(urlsafe_b64decode(raw.encode("utf-8")).decode("utf-8")))
+
 
 @app.route("/api/user/auth", methods=["POST"])
 async def authUser():
@@ -46,13 +53,14 @@ async def authUser():
         return "Invalid"
     # add new user if key isn't in database
     user = Users(
-        usn = data["usn"],
-        pwdHash = bcrypt.hashpw(data["pwd"].encode("utf-8"), bcrypt.gensalt()),
-        pubKey = data["pubKey"],
+        usn=data["usn"],
+        pwdHash=bcrypt.hashpw(data["pwd"].encode("utf-8"), bcrypt.gensalt()),
+        pubKey=data["pubKey"],
     )
     db.session.add(user)
     db.session.commit()
     return "Registered"
+
 
 @app.route("/api/user/id", methods=["POST"])
 async def uid():
@@ -63,6 +71,7 @@ async def uid():
         return str(storedUser[0].uid)
     return f"User {data['usn']} not found"
 
+
 @app.route("/api/user/usn", methods=["POST"])
 async def usn():
     data = flask.request.get_json()
@@ -72,29 +81,83 @@ async def usn():
         return storedUser[0].usn
     return f"User {data['uid']} not found"
 
-@app.route("/api/bubble/new")
+@app.route("/api/user/pubKey", methods=["POST"])
+async def upubkey():
+    data = flask.request.get_json()
+    query = db.select(Users).where(Users.uid == data["uid"])
+    storedUser = db.session.execute(query).fetchone()
+    if storedUser != None:
+        return storedUser[0].pubKey
+    return f"User {data['uid']} not found"
+
+
+@app.route("/api/bubble/new", methods=["POST"])
 async def newRoom():
-    raw = flask.request.args.get("data")
-    data = decode(raw)
+    data = flask.request.get_json()
     room = Bubbles(
-        uids = json.dumps(data["uids"])
+        uid=data["uid"]
     )
     db.session.add(room)
     db.session.commit()
-    return "Created"
+    db.session.refresh(room)
+    # return the bid
+    return str(room.bid)
+
+async def checkRoomExist(bid):
+    # returns True if room exists in db, False if not
+    query = db.select(Bubbles).where(Bubbles.bid == bid)
+    bubbles = db.session.execute(query).fetchone()
+    return len(bubbles) > 0
+
+async def checkUserInRoom(bid, uid):
+    # returns True if user is in the room, returns False is not
+    query = db.select(Bubbles).where(Bubbles.bid == bid, Bubbles.uid == uid)
+    bubbles = db.session.execute(query).fetchone()
+    return len(bubbles) != 0
+
+@app.route("/api/bubble/invite", methods=["POST"])
+async def inviteToRoom():
+    data = flask.request.get_json()
+    # write a verification that the person making the request is actually in the room
+    if not await checkRoomExist(data["bid"]):
+        return f"Bubble {data['bid']} not found"
+    if await checkUserInRoom(data["bid"], data["uid"]):
+        return f"User {data['uid']} already in bubble"
+    room = Bubbles(
+        bid=data["bid"],
+        uid=data["uid"]
+    )
+    db.session.add(room)
+    db.session.commit()
+    return "Success"
+
 
 @app.route("/api/bubble/uids", methods=["POST"])
-async def bubble_uids():
-    raw = flask.request.args.get("data")
-    data = decode(raw)
+async def bubbleUids():
+    data = flask.request.get_json()
     query = db.select(Bubbles).where(Bubbles.bid == int(data["bid"]))
-    storedBubble = db.session.execute(query).fetchone()
+    storedBubble = db.session.execute(query).fetchall()
     if storedBubble != None:
-        return storedBubble[0].uids
+        return [bubble[0].uid for bubble in storedBubble]
     return f"Bubble {data['bid']} not found"
+
+
+@app.route("/api/msg/commit", methods=["POST"])
+async def msgCommit():
+    data = flask.request.get_json()
+    newMsg = Messages(
+        authUID=data["authUID"],
+        bid=data["bid"],
+        content=data["content"],
+        sig=data["sig"]
+    )
+    # TODO add measures to verify signature before commiting
+    db.session.add(newMsg)
+    db.session.commit()
+    return "Created"
 
 if __name__ == "__main__":
     db.init_app(app)
     with app.app_context():
         db.create_all()
-    app.run() # comment out in production
+    app.run()  # comment out in production
